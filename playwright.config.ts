a568bd5b0d29409386587bfc127425ccc1be62d0
NeoCode <kresnayuda29@google.com>
2026-08-05 12:33:51 +0700

    fix(e2e): uncomment webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
